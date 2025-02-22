22b92d6cefc92aaec0028a178417bda318a9af78
M Thomason <matt@warpedcore.net>
2025-02-22 22:02:41 +0000

    Extract characterSpecialization from APICharacterSpecializationsSummary

--- src/profile/character-specializations.ts
+++ src/profile/character-specializations.ts
@@ -14,31 +14,33 @@ import { linksStruct, refStruct, characterRef, spellTooltip } from "../shared";
  */
 export interface APICharacterSpecializationsSummary {
     _links: linksStruct;
-    specializations: {
-        specialization: refStruct;
-        glyphs?: refStruct[];
-        pvp_talent_slots?: {
-            selected: {
-                talent: refStruct;
-                spell_tooltip: spellTooltip;
-            }
-            slot_number: number;
-        }[];
-        loadouts: {
-            is_active: boolean;
-            talent_loadout_code: string;
-            selected_class_talents: {
-                id: number;
-                rank: number;
-                tooltip?: {
-                    talent: refStruct;
-                    spell_tooltip: spellTooltip;
-                }
-            }
-        }[];
-    }[]
+    specializations: characterSpecialization[];
     active_specialization: refStruct;
     character: characterRef;
     active_hero_talent_tree: refStruct;
+}
+
+export interface characterSpecialization{
+    specialization: refStruct;
+    glyphs?: refStruct[];
+    pvp_talent_slots?: {
+        selected: {
+            talent: refStruct;
+            spell_tooltip: spellTooltip;
+        }
+        slot_number: number;
+    }[];
+    loadouts: {
+        is_active: boolean;
+        talent_loadout_code: string;
+        selected_class_talents: {
+            id: number;
+            rank: number;
+            tooltip?: {
+                talent: refStruct;
+                spell_tooltip: spellTooltip;
+            }
+        }
+    }[];
 }
 
